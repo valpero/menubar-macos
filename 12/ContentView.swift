fe677c53c8e2ee2d42a69132172c1f44fcc453c5
CyberScoper <aleksandr@murzin.digital>
2026-04-05 04:18:51 +0200

    Initial Commit

---
+++ 12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  12
+//
+//  Created by aleksandr on 05/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 12/_2App.swift
@@ -0,0 +1,17 @@
+//
+//  _2App.swift
+//  12
+//
+//  Created by aleksandr on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct _2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
